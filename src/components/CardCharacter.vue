0fe8e5f0dd3743848f3969556bd6562b6504a6d8
51lv3r258 <jrp8900@gmail.com>
2021-10-07 19:25:11 -0500

    ✨ Obteniendo datos de un api de Rick and Morty

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="character">
+    <div class="character_info">
+      {{ character.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    character: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <div class="characters">
+      <div
+        class="characters_item"
+        v-for="character in characters"
+        :key="character.id"
+      >
+        <CardCharacter :character="character" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+
+import CardCharacter from "@/components/CardCharacter";
+
+export default {
+  components: {
+    CardCharacter,
+  },
+  setup() {
+    const store = useStore();
+
+    const characters = computed(() => store.state.charactersFilter);
+
+    onMounted(() => {
+      store.dispatch("getCharacters");
+    });
+
+    return {
+      characters,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
